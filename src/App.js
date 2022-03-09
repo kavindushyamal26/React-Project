844f0ac3bcee259a25ecdc63f2833c27a2114b2c
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-03-09 12:09:37 +0530

    Initiate routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import "./App.css";
-import { Component } from "react";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { computeHeadingLevel } from "@testing-library/react";
 import _ from "lodash";
+import "./App.css";
 import MainCounter from "./components/maincounter";
+import Users from "./components/users";
+import Orders from "./components/orders";
+import Notfound from "./components/notfound";
 
 class App extends Component {
   render() {
     return (
       <>
-        <MainCounter />
+        <main className="container">
+          <Switch>
+            <Route path="/maincounter" component={MainCounter}></Route>
+            <Route path="/users" component={Users}></Route>
+            <Route path="/orders" component={Orders}></Route>
+            <Route path="/notfound" component={Notfound}></Route>
+            <Redirect from="/" exact to="/maincounter" />
+            <Redirect to="/notfound" />
+          </Switch>
+        </main>
       </>
     );
   }
