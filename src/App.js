84fceeaff3a99a7414bf0dae5e38705aedf9180d
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-03-13 07:07:47 +0530

    Fixed navbar and lifiting state up

--- src/App.js
+++ src/App.js
@@ -2,21 +2,117 @@ import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { computeHeadingLevel } from "@testing-library/react";
 import _ from "lodash";
-import "./App.css";
+import Navbar from "./components/navbar";
 import MainCounter from "./components/maincounter";
 import Users from "./components/users";
 import Orders from "./components/orders";
 import Notfound from "./components/notfound";
 import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 2, like: false, color: "Green" },
+      { id: 2, value: 0, like: false, color: "Red" },
+      { id: 3, value: 0, like: false, color: "Yellow" },
+      { id: 4, value: 0, like: false, color: "Green" },
+      { id: 5, value: 0, like: false, color: "Green" },
+      { id: 6, value: 0, like: false, color: "Yellow" },
+      { id: 7, value: 0, like: false, color: "Red" },
+      { id: 8, value: 0, like: false, color: "Yellow" },
+    ],
+    pageSize: 3,
+    currentPage: 1,
+    selectedColor: "",
+    sortColumn: { path: "title", order: "asc" },
+  };
+
+  handleDelete = (cid) => {
+    const counters = this.state.counters.filter((c) => c.id !== cid);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleLike = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].like = !counters[index].like;
+    this.setState({ counters });
+    //console.log("like clicked", counter);
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  handleColorSelect = (color) => {
+    //console.log("color", color);
+    this.setState({ selectedColor: color, currentPage: 1 });
+  };
+
+  handleSort = (path) => {
+    //console.log("column", column);
+    this.setState({ sortColumn: { path, order: "asc" } });
+  };
+
   render() {
+    //   const filtered =
+    //   selectedColor && selectedColor != "All"
+    //     ? this.state.counters.filter((c) => c.color === selectedColor)
+    //     : allCounters;
+
+    // // const sorted = _.groupBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    // // const counters = paginate(filtered, currentPage, pageSize);
+
     return (
       <>
-        <main>
+        <Navbar
+          totalCount={this.state.counters.filter((c) => c.value > 0).length}
+          totalLike={this.state.counters.filter((c) => c.like === true).length}
+          // countersCount={filtered.length}
+        />
+        <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm}></Route>
-            <Route path="/maincounter" component={MainCounter}></Route>
+            <Route
+              path="/maincounter"
+              render={(props) => (
+                <MainCounter
+                  counters={this.state.counters}
+                  pageSize={this.state.pageSize}
+                  currentPage={this.state.currentPage}
+                  sortColumn={this.state.sortColumn}
+                  selectedColor={this.state.selectedColor}
+                  handleDelete={this.handleDelete}
+                  handleIncrement={this.handleIncrement}
+                  handleDecrement={this.handleDecrement}
+                  handleLike={this.handleLike}
+                  handlePageChange={this.handlePageChange}
+                  handleColorSelect={this.handleColorSelect}
+                  handleSort={this.handleSort}
+                  filtered={this.filtered}
+                  {...props}
+                />
+              )}
+            ></Route>
             <Route path="/users" component={Users}></Route>
             <Route path="/orders" component={Orders}></Route>
             <Route path="/notfound" component={Notfound}></Route>
--- src/components/maincounter.jsx
+++ src/components/maincounter.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Navbar from "./navbar";
 import Counters from "./counters";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
@@ -8,73 +7,12 @@ import SortGroup from "./common/sortGroup";
 import _ from "lodash";
 
 class MainCounter extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 2, like: false, color: "Green" },
-      { id: 2, value: 0, like: false, color: "Red" },
-      { id: 3, value: 0, like: false, color: "Yellow" },
-      { id: 4, value: 0, like: false, color: "Green" },
-      { id: 5, value: 0, like: false, color: "Green" },
-      { id: 6, value: 0, like: false, color: "Yellow" },
-      { id: 7, value: 0, like: false, color: "Red" },
-      { id: 8, value: 0, like: false, color: "Yellow" },
-    ],
-    pageSize: 3,
-    currentPage: 1,
-    sortColumn: { path: "title", order: "asc" },
-  };
-
-  handleDelete = (cid) => {
-    //console.log("delete", cid);
-    const counters = this.state.counters.filter((c) => c.id !== cid);
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
   handleReset = () => {
-    const counters = this.state.counters.map((c) => {
+    const counters = this.props.counters.map((c) => {
       c.value = 0;
       return c;
     });
     this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
-  };
-
-  handleLike = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].like = !counters[index].like;
-    this.setState({ counters });
-    //console.log("like clicked", counter);
-  };
-
-  handlePageChange = (page) => {
-    this.setState({ currentPage: page });
-  };
-
-  handleColorSelect = (color) => {
-    //console.log("color", color);
-    this.setState({ selectedColor: color, currentPage: 1 });
-  };
-
-  handleSort = (path) => {
-    //console.log("column", column);
-    this.setState({ sortColumn: { path, order: "asc" } });
   };
 
   render() {
@@ -84,61 +22,52 @@ class MainCounter extends Component {
       sortColumn,
       selectedColor,
       counters: allCounters,
-    } = this.state;
+      handleIncrement,
+      handleDecrement,
+      handleDelete,
+      handleLike,
+      handlePageChange,
+      handleColorSelect,
+      handleSort,
+    } = this.props;
 
-    //console.log("selcol", selectedColor);
     const filtered =
       selectedColor && selectedColor != "All"
         ? allCounters.filter((c) => c.color === selectedColor)
         : allCounters;
-    //console.log("filtered", filtered);
 
-    console.log("sortcolum", [sortColumn.path]);
     const sorted = _.groupBy(filtered, [sortColumn.path], [sortColumn.order]);
-    //const sorted = filtered.sort((a, b) => a.color > b.color);
-    console.log("filtered", filtered);
-    console.log("Sorted", sorted);
 
     const counters = paginate(filtered, currentPage, pageSize);
 
     return (
       <>
-        <Navbar
-          totalCount={this.state.counters.filter((c) => c.value > 0).length}
-          totalLike={this.state.counters.filter((c) => c.like === true).length}
-          countersCount={filtered.length}
-        />
-        <div className="container">
-          <div className="row">
-            <div className="col-2 m-3">
-              <ListGroup
-                counters={this.state.counters}
-                selectedItem={this.state.selectedColor}
-                onItemSelect={this.handleColorSelect}
-              />
-            </div>
-            <div className="col-2 m-3">
-              <SortGroup
-                counters={this.state.counters}
-                onSort={this.handleSort}
-              />
-            </div>
-            <div className="col">
-              <Counters
-                onDelete={this.handleDelete}
-                onIncrement={this.handleIncrement}
-                onDecrement={this.handleDecrement}
-                onReset={this.handleReset}
-                onLike={this.handleLike}
-                counters={counters}
-              />
-              <Pagination
-                itemsCount={filtered.length}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageChange={this.handlePageChange}
-              />
-            </div>
+        <div className="row">
+          <div className="col-2 m-3">
+            <ListGroup
+              counters={allCounters}
+              selectedItem={selectedColor}
+              onItemSelect={handleColorSelect}
+            />
+          </div>
+          <div className="col-2 m-3">
+            <SortGroup counters={allCounters} onSort={handleSort} />
+          </div>
+          <div className="col">
+            <Counters
+              onDelete={handleDelete}
+              onIncrement={handleIncrement}
+              onDecrement={handleDecrement}
+              onReset={this.handleReset}
+              onLike={handleLike}
+              counters={counters}
+            />
+            <Pagination
+              itemsCount={filtered.length}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
           </div>
         </div>
       </>
