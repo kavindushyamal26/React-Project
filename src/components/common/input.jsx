7dbae50437f0e6cdc95393fe13fd17c570f120a3
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-03-14 11:27:29 +0530

    Initiating validation for the login form

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, autofocus }) => {
+const Input = ({ name, label, value, onChange, autofocus, error }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
@@ -13,6 +13,7 @@ const Input = ({ name, label, value, onChange, autofocus }) => {
         className="form-control"
         autoFocus={autofocus}
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,21 +4,55 @@ import Input from "./common/input";
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
+  validateProperty = (input) => {
+    if (input.name === "username") {
+      if (input.value.trim() === "") return "Username is required";
+    }
+
+    if (input.name === "password") {
+      if (input.value.trim() === "") return "Password is required";
+    }
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log("E :", errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     //console.log("submited");
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <main className="container">
         <h1>Login</h1>
@@ -29,6 +63,7 @@ class LoginForm extends Component {
             value={account.username}
             onChange={this.handleChange}
             autofocus={true}
+            error={errors.username}
           />
           <Input
             name="password"
@@ -36,6 +71,7 @@ class LoginForm extends Component {
             value={account.password}
             onChange={this.handleChange}
             autofocus={false}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
