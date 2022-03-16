4110d09c998aa304ddba777e82e4e17504e60f38
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-03-16 11:23:56 +0530

    Disabling login button

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -99,7 +99,9 @@ class LoginForm extends Component {
             autofocus={false}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </main>
     );
