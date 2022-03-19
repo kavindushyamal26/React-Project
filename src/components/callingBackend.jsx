61c7f26659225ad387dc5fad7a463ee5d2430083
Kavindu Shyamal <shyamald_im17073@stu.kln.ac.lk>
2022-03-19 12:40:34 +0530

    Getting data from backend server and display on grid

--- src/components/callingBackend.jsx
+++ src/components/callingBackend.jsx
@@ -2,16 +2,61 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class CallingBackEnd extends Component {
-  state = {};
+  state = { posts: [] };
 
   async componentDidMount() {
-    const response = await axios.get(
+    const { data: posts } = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    console.log("response", response);
+    this.setState({ posts });
+    //console.log("Data", data);
   }
+
+  handleAdd = () => {
+    console.log("Add");
+  };
+
   render() {
-    return <h1>BackEnd</h1>;
+    return (
+      <>
+        <h1 className="mb-4">BackEnd</h1>
+        <button
+          type="button"
+          class="btn btn-success mb-4"
+          onClick={this.handleAdd}
+        >
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Update</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((p) => (
+              <tr>
+                <th scope="row">{p.id}</th>
+                <td>{p.title}</td>
+                <td>
+                  <button type="button" class="btn btn-info">
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button type="button" class="btn btn-danger">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
